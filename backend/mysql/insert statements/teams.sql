34a29af06111a1eefc979a8010cf4c3be06e1b56
Eli <elisego45@gmail.com>
2020-03-28 20:58:47 -0500

    Working with James'new Data

---
+++ backend/mysql/insert statements/teams.sql
@@ -0,0 +1,8 @@
+insert into teams (id, name) values (1, 'Dallas Renegades');
+insert into teams (id, name) values (2, 'Houston Roughnecks');
+insert into teams (id, name) values (3, 'Los Angeles Wildcats');
+insert into teams (id, name) values (4, 'New York Guardians');
+insert into teams (id, name) values (5, 'St. Louis BattleHawks');
+insert into teams (id, name) values (6, 'Seattle Dragons');
+insert into teams (id, name) values (7, 'Tampa Bay Vipers');
+insert into teams (id, name) values (8, 'D.C. Defenders');
