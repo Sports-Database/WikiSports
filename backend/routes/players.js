9dec7d8bd7cc422302ce3675a1f94fec83fbe2f5
Eli <elisego45@gmail.com>
2020-03-28 23:04:27 -0500

    Making changes to routes

--- backend/routes/players.js
+++ backend/routes/players.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const Router = express.Router()
 const mysqlConnection = require("../connections")
+
+
 Router.get("/", (req,res) =>{
     console.log("Doing something")
     mysqlConnection.query("SELECT * FROM players;" , (err, rows, fields)=>{
@@ -8,6 +10,40 @@ Router.get("/", (req,res) =>{
         res.end(JSON.stringify(rows));
     })
     console.log("Got out of query")
+})
+
+Router.get("/:playerName", (req,res) =>{
+    let pName = req.params.playerName
+    let arr = pName.split(' ')
+    mysqlConnection.query(`
+        SELECT p.id, p.name, p.position, 
+            (select count(*) from touchdowns 
+            where touchdowns.playerId = p.id) as 'total_games',
+            sum(td.touchdowns) 'touchdowns', sum(y.yards) 'yards',
+            sum(t.tackles) 'tackles', sum(f.fumbles) 'fumbles',
+            sum(e.earnings) 'earnings'
+        FROM players p
+        INNER JOIN
+        touchdowns td
+        on p.id = td.playerID
+        INNER JOIN
+        yards y
+        on p.id = y.playerID
+        INNER JOIN
+        tackles t
+        on p.id = t.playerId
+        INNER JOIN 
+        fumbles f
+        on p.id = f.playerId
+        INNER JOIN
+        earnings e
+        on p.id = e.playerId
+        WHERE players.name = ${arr[1]}, ${arr[0]};
+        group by p.id;` , 
+        (err, rows, fields)=>{
+            if(err) throw err
+            res.end(JSON.stringify(rows));
+        })
 })
 
 module.exports = Router
--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,7 @@ var mysql = require('mysql')
 const acronymRoutes = require("./routes/acronyms")
 const leagueRoutes = require("./routes/league")
 const playerRoutes = require("./routes/players")
-const playerStatRoutes = require("./routes/playerStats")
 const teamRoutes = require("./routes/teams")
-const teamStatsRoutes = require("./routes/teamStats")
 
 
 
@@ -18,9 +16,7 @@ app.use(express.json());
 app.use("/acronyms", acronymRoutes)
 app.use("/league", leagueRoutes)
 app.use("/players", playerRoutes)
-app.use("/playerStats", playerStatRoutes)
 app.use("/teams", teamRoutes)
-app.use("/teamStats", teamStatsRoutes)
 
 
 //PORT ENVIRONMENT VARIABLE
