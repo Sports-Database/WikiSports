ad95ae68e723c665d64882572905c290203ef5e9
jameshhe <hhe990219@hotmail.com>
2020-03-28 19:58:05 -0500

    Added insert statements and fixed web scrape

--- ScrapePlayers.py
+++ ScrapePlayers.py
@@ -8,6 +8,7 @@ from random import randint
 
 # list that contains all the players
 players = []
+positions = []
 letters = list(string.ascii_uppercase)
 
 # scrape players starting with every letter
@@ -15,7 +16,7 @@ for letter in letters:
     URL = "https://www.footballdb.com/xfl/players.html?letter=" + letter
     req = Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
     # make the request rate lower so we don't flood the server
-    sleep(randint(8, 15))
+    sleep(randint(1, 5))
     page = urlopen(req).read()
 
     soup = BeautifulSoup(page, 'html.parser')
@@ -24,13 +25,17 @@ for letter in letters:
     playerTb = soup.find('table', class_='statistics scrollable')
     # for each row of player data
     for row in playerTb.find_all('tr'):
-        name = [r.text for r in row.find_all('td')[:1]]
+        name = ''.join([r.text for r in row.find_all('td')[:1]]).strip('')
+        position = ''.join([r.text for r in row.find_all('td')[1:2]]).strip('')
         players.append(name)
+        positions.append(position)
 
 
 # filter the blank strings
 players = list(filter(None, players))
-with open('output.csv', 'w') as csv_file:
+positions = list(filter(None, positions))
+with open('output.csv', 'w', newline='') as csv_file:
     writer = csv.writer(csv_file)
-    for p in players:
-        writer.writerow(p)
+    writer.writerow(['id', 'name', 'position'])
+    for playerId, player in enumerate(players):
+        writer.writerow([playerId, player, positions[playerId]])
---
+++ insert statements/league.sql
@@ -0,0 +1,3 @@
+insert into league (seasonId, MVPId, passingLeaderId, rushingLeaderId, touchdownLeaderId, championId) values (1, 211, 393, 342, 234, 5);
+insert into league (seasonId, MVPId, passingLeaderId, rushingLeaderId, touchdownLeaderId, championId) values (2, 99, 312, 73, 379, 5);
+insert into league (seasonId, MVPId, passingLeaderId, rushingLeaderId, touchdownLeaderId, championId) values (3, 327, 200, 297, 141, 6);
