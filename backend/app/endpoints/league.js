b7eebed931743e84636bdb849e8ffb0d9a370a1a
Eli <elisego46@gmail.com>
2020-04-16 15:50:33 -0500

    Added endpoints to get leaders of every category

--- backend/app/endpoints/league.js
+++ backend/app/endpoints/league.js
@@ -1,7 +1,127 @@
 const express = require("express")
-const Router = express.Router()
+const router = express.Router()
 const mysqlConnection = require("../connections")
 
 
+// gets the leader in rebounds for every season
+router.get('/rebounds', (req,res) => {
+    mysqlConnection.query(`
+        SELECT 
+            s.season,
+            p.name,
+            rebounds
+        FROM league
+        INNER JOIN players p ON p.id = reboundLeaderId
+        INNER JOIN 
+            (SELECT  p.name, seasonId, SUM(Rebounds) as rebounds
+            from stats s
+            inner join games g ON g.GameId = s.GameId
+            inner join players p ON p.id = s.playerId
+            group by p.name, seasonId) as allRebounds
+        ON allRebounds.seasonId = league.seasonId AND allRebounds.name = p.name
+        INNER JOIN seasons s ON s.id = league.seasonId;
+    `,
+      (err, rows, fields) => {
+        if(err) throw err
+        res.end(JSON.stringify(rows));
+    })// query
+  })// 
 
-module.exports = Router // export
+
+  // gets best points for player of every season
+  router.get('/points', (req,res) => {
+    mysqlConnection.query(`
+    SELECT 
+        s.season,
+        p.name,
+        points,
+        p.url
+    FROM league
+    INNER JOIN players p ON p.id = scoringLeaderId
+    INNER JOIN 
+        (SELECT  p.name, seasonId, SUM(Points) as points
+        from stats s
+        INNER JOIN games g ON g.GameId = s.GameId
+        INNER JOIN players p ON p.id = s.playerId
+        GROUP BY p.name, seasonId) as allPoints
+    ON allPoints.seasonId = league.seasonId AND allPoints.name = p.name
+    INNER JOIN seasons s ON s.id = league.seasonId;
+    `,
+      (err, rows, fields) => {
+        if(err) throw err
+        res.end(JSON.stringify(rows));
+    })// query
+  })// 
+
+    // gets best assist player for every season
+  router.get('/assists', (req,res) => {
+    mysqlConnection.query(`
+    SELECT 
+        s.season,
+        p.name,
+        assists,
+        p.url
+    FROM league
+    INNER JOIN players p ON p.id = assistLeaderId
+    INNER JOIN 
+        (SELECT  p.name, seasonId, SUM(Assists) as assists
+        from stats s
+        inner join games g on g.GameId = s.GameId
+        inner join players p on p.id = s.playerId
+        group by p.name, seasonId) as allAssists
+    ON allAssists.seasonId = league.seasonId AND allAssists.name = p.name
+    INNER JOIN seasons s on s.id = league.seasonId;
+    `,
+      (err, rows, fields) => {
+        if(err) throw err
+        res.end(JSON.stringify(rows));
+    })// query
+  })// 
+
+
+   // gets best steals for every season
+  router.get('/steals', (req,res) => {
+    mysqlConnection.query(`
+    SELECT
+        se.season,
+        p.name,
+        maxxSteals as steals,
+        p.url
+    FROM players p
+    INNER JOIN
+    (SELECT DISTINCT seasonId, maxxSteals, max(pId) as pId
+        FROM
+        (SELECT seasonId sId, playerId pId, sum(steals) sSteals 
+        FROM stats thisStats
+        INNER JOIN games gg on gg.GameId = thisStats.GameId
+        group by sId, pId) as stealSum
+        INNER JOIN
+        (select seasonId, max(steals) maxxSteals
+        FROM
+            (select seasonId, playerId, sum(steals) as steals from stats s
+            inner join games g on g.GameId = s.GameId
+            group by seasonId, playerId) as mostSteals
+        group by seasonId) as maxSteals
+        ON maxSteals.maxxSteals = sSteals
+        group by seasonId, maxxSteals) as steals
+    ON steals.pId = p.id
+    INNER JOIN seasons se
+    ON se.id = seasonId;
+    `,
+      (err, rows, fields) => {
+        if(err) throw err
+        res.end(JSON.stringify(rows));
+    })// query
+  })// 
+
+
+
+  
+  
+
+
+
+
+
+
+module.exports = router // export
