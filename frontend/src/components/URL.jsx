b6131df93b7b4946344b88339309d6cf2fb31769
Eli <elisego45@gmail.com>
2020-04-29 17:12:41 -0500

    Removed repetition

--- frontend/src/components/URL.jsx
+++ frontend/src/components/URL.jsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react'
 import { Table, Container } from 'react-bootstrap'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-const APIURL  = 'http://localhost:8080'
-const HOMEURL = 'http://localhost:80'
+const url = 'localhost'
+const base = 'http://'+url
+const APIURL  = base+':8080'
+const HOMEURL = base+':80'
 
 export {APIURL}
 export {HOMEURL}
