422fb6da0df3d9dbdcae6726d050f44a8a064c64
Eli <47865195+elisego45@users.noreply.github.com>
2020-04-30 20:06:38 -0500

    Update URL.jsx

--- frontend/src/components/URL.jsx
+++ frontend/src/components/URL.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Table, Container } from 'react-bootstrap'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-const url = '192.168.1.17'
+const url = 'localhost'
 const base = 'http://'+url
 const APIURL  = base+':8080'
 const HOMEURL = base+':80'
